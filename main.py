725fc463868d33c44453654edeb14bbf82e4ac9b
Buco <80671351+Bucolo@users.noreply.github.com>
2021-09-26 17:17:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,11 @@ from quart import Quart,jsonify,request,current_app
 from werkzeug.exceptions import HTTPException
 from discord.ext import tasks
 import copy
-
+from uvicorn.middleware.proxy_headers import ProxyHeadersMiddleware
 loop=asyncio.get_event_loop()
 app = Quart(__name__)
+
+app.asgi_app = ProxyHeadersMiddleware(app.asgi_app, trusted_hosts=["127.0.0.1"])
 
 with open("json/credentials.json",'r') as f:
     dt=json.load(f)
@@ -60,25 +62,32 @@ def methodandimage(method,image,user_id):
         return "DELETE FROM FavImages WHERE user_id=%s and image=%s",args
     return None,None
 
-async def is_valid_token(token_header):
+async def is_valid_token(token_header,request_perms=None):
+    if not token_header:
+        return quart.abort(401,description="No Token, please check that you provided a Token and that your correctly requested the route with a trailing slash (the Authorization header being not kept if you didn't).")
+
     try:
         token = token_header.split(" ")[1]
         rule = URLSafeSerializer(app.secret_key)
         info=rule.loads(token)
-        user_secret=info["secret"]
-        user_id=int(info['id'])
+        user_secret=info.get("secret")
+        user_id=int(info.get('id'))
     except (TypeError,KeyError,AttributeError,IndexError,BadSignature):
-        quart.abort(401,description="Invalid Token, please check that your token is up to date or that you did correctly format it in the Authorization header.")
+        quart.abort(403,description=f"Invalid Token, please check that you did correctly format it in the Authorization header and that the token is up to date.")
 
     else:
         async with app.pool.acquire() as conn:
             async with conn.cursor() as cur:
-                await cur.execute("SELECT id from User WHERE id=%s and secret=%s ",(user_id,user_secret))
+                if request_perms:
+                    perm_name="access_galleries"
+                    await cur.execute("SELECT User.is_admin,Permissions.page FROM User LEFT JOIN Permissions ON Permissions.user_id=User.id WHERE User.id=%s and User.secret=%s and (Permissions.page=%s or User.is_admin) ",(user_id,user_secret,perm_name))
+                else:
+                    await cur.execute("SELECT id,is_admin from User WHERE id=%s and secret=%s ",(user_id,user_secret))
                 authorized=await cur.fetchone()
                 if authorized:
                     return True
                 else:
-                    quart.abort(401,description="Invalid Token, please check that your token is up to date or that you did correctly format it in the Authorization header.")
+                    quart.abort(403,description=f"Invalid Token, You do not have the permissions to request this route please check that the token is up to date{' and, as you requested the id url parameter that you have the permissions to do so' if request_perms else ''}.")
 
 """error handlers"""
 
@@ -100,7 +109,7 @@ def requires_token_authorization(view):
 
     @functools.wraps(view)
     async def wrapper(*args, **kwargs):
-        await is_valid_token(request.headers.get('Authorization'))
+        await is_valid_token(request.headers.get('Authorization'),request_perms=request.args.get('id'))
         return await view(*args, **kwargs)
     return wrapper
 
@@ -164,13 +173,13 @@ async def principal(typ,categorie):
                 else:
                     gifstr=" and not Images.extension='.gif'"
                 if banned_files:
-                    await cur.execute(f"""SELECT Images.file,Images.extension,Tags.id,Tags.name FROM LinkedTags
+                    await cur.execute(f"""SELECT Images.file,Images.extension,Images.dominant_color,Tags.id,Tags.name FROM LinkedTags
                                     JOIN Images ON Images.file=LinkedTags.image
                                     JOIN Tags ON Tags.id=LinkedTags.tag_id
                                     WHERE not Images.is_banned and not Images.under_review and (Tags.id=%s or Tags.name=%s) and Tags.is_over18={1 if over18 else 0}{gifstr} and LinkedTags.image not in %s{' GROUP BY LinkedTags.image' if many else ''}
                                     ORDER BY RAND() LIMIT {'30' if many else '1'}""",(categorie,categorie,banned_files))
                 else:
-                    await cur.execute(f"""SELECT Images.file,Images.extension,Tags.id,Tags.name FROM LinkedTags
+                    await cur.execute(f"""SELECT Images.file,Images.extension,Images.dominant_color,Tags.id,Tags.name FROM LinkedTags
                                     JOIN Images ON Images.file=LinkedTags.image
                                     JOIN Tags ON Tags.id=LinkedTags.tag_id
                                     WHERE not Images.is_banned and not Images.under_review and (Tags.id=%s or Tags.name=%s) and Tags.is_over18={1 if over18 else 0}{gifstr}{' GROUP BY LinkedTags.image' if many else ''}
@@ -179,16 +188,19 @@ async def principal(typ,categorie):
                 fetch=list(await cur.fetchall())
                 file=[]
                 picture=[]
+                dominant_color=[]
                 for im in fetch:
                     file.append(im["file"]+im["extension"])
                     picture.append("https://api.waifu.im/image/"+im["file"]+im["extension"])
+                    dominant_color.append(im['dominant_color'])
                 if len(picture)<1:
                     print(f"This request for {categorie} ended in criteria error.")
                     quart.abort(404,description=f"Sorry there is no {typ} image matching your criteria with the tag : {categorie}. Please change the criteria or consider changing your tag.")
                 tag_id=fetch[0]['id']
                 tag_name=fetch[0]['name']
-
-                data={'code':200,'is_over18':over18,'tag_id':tag_id,'tag_name':tag_name,'file':file if len(file)>1 else file[0],'url':picture if len(picture)>1 else picture[0]}
+                print(f'File: {file if len(file)>1 else file[0]}')
+                
+                data={'code':200,'is_over18':over18,'tag_id':tag_id,'tag_name':tag_name,'file':file if len(file)>1 else file[0],'dominant_color':dominant_color if len(dominant_color)>1 else dominant_color[0],'url':picture if len(picture)>1 else picture[0]}
                 return jsonify(data)
 
     return quart.abort(404,f"Sorry there isn't any type named : {typ}. Please retry with either {' or '.join(autho)}.")
@@ -201,7 +213,8 @@ async def fav_():
     rule = URLSafeSerializer(app.secret_key)
     info=rule.loads(token)
     user_secret=info["secret"]
-    user_id=int(info['id'])
+    rqst_user=request.args.get('id')
+    user_id=rqst_user if rqst_user else int(info['id'])
     #add or remove image part
     querys=[]
     insert=request.args.get('insert')
@@ -214,7 +227,7 @@ async def fav_():
         async with conn.cursor(aiomysql.DictCursor) as cur:
             for query in querys:
                 await cur.executemany(query[0],query[1])
-            await cur.execute("""SELECT Images.extension,Tags.name,Tags.id,Tags.is_over18,Tags.description,Images.file FROM FavImages
+            await cur.execute("""SELECT Images.extension,Tags.name,Tags.id,Tags.is_over18,Tags.description,Images.file,Images.dominant_color FROM FavImages
                                 JOIN Images ON Images.file=FavImages.image
                                 JOIN LinkedTags ON LinkedTags.image=FavImages.image
                                 JOIN Tags on LinkedTags.tag_id=Tags.id
@@ -225,20 +238,24 @@ async def fav_():
         quart.abort(404,description="You have no Gallery or it is now empty.")
     all_u=[]
     all_f=[]
+    all_d=[]
     tags_nsfw={}
     tags_sfw={}
     default_tags={'ero':tags_nsfw,'all':tags_sfw}
     for im in images:
         filename=im['file']+im["extension"]
         url=f"https://api.waifu.im/image/{filename}"
+        domincolor=im['dominant_color']
         if not im["is_over18"]:
             if not im["name"] in tags_sfw:
                 newtag=copy.deepcopy(im)
                 del newtag['extension']
                 newtag['file']=[]
+                newtag['dominant_color']=[]
                 newtag['is_over18']=True if newtag['is_over18'] else False
                 newtag.update({'url':[]})
                 tags_sfw[im["name"]]=newtag
+            tags_sfw[im["name"]]['dominant_color'].append(domincolor)
             tags_sfw[im["name"]]['url'].append(url)
             tags_sfw[im["name"]]['file'].append(filename)
         else:
@@ -246,23 +263,27 @@ async def fav_():
                 newtag=copy.deepcopy(im)
                 del newtag['extension']
                 newtag['file']=[]
+                newtag['dominant_color']=[]
                 newtag['is_over18']=True if newtag['is_over18'] else False
                 newtag.update({'url':[]})
                 tags_nsfw[im["name"]]=newtag
+            tags_nsfw[im["name"]]['dominant_color'].append(domincolor)
             tags_nsfw[im["name"]]['url'].append(url)
             tags_nsfw[im["name"]]['file'].append(filename)
 
     files={}
     if tags_sfw:
         all_f.extend(tags_sfw['all']['file'])
+        all_d.extend(tags_sfw['all']['dominant_color'])
         all_u.extend(tags_sfw['all']['url'])
         files.update({'sfw':tags_sfw})
     if tags_nsfw:
-        all_f.extend(tags_sfw['all']['file'])
-        all_u.extend(tags_sfw['all']['url'])
+        all_f.extend(tags_nsfw['ero']['file'])
+        all_d.extend(tags_nsfw['ero']['dominant_color'])
+        all_u.extend(tags_nsfw['ero']['url'])
         files.update({'nsfw':tags_nsfw})
     if all_f and all_u:
-        files.update({'file':all_f,'url':all_u})
+        files.update({'file':all_f,'dominant_color':all_d,'url':all_u})
     return jsonify(files)
 
 """endpoints with and without info"""
