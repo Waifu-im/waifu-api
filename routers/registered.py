87b552d67663bc0f868326ea612342e69eec7d09
Buco <80671351+Bucolo@users.noreply.github.com>
2021-11-26 00:19:41 +0100

    Update registered.py

--- routers/registered.py
+++ routers/registered.py
@@ -15,6 +15,8 @@ async def fav_(request:Request,user_id:int=None,authorization: str = Header(None
     username=None
     if user_id:
         resp=await request.app.state.httpsession.get(f'http://127.0.0.1:8033/userinfos/?id={user_id}')
+        if resp.status==404:
+            raise HTTPException(status_code=400,detail="Please provide a valid user_id")
         if resp.status!=200:
             raise HTTPException(status_code=500,detail="Sorry, something went wrong with the ipc request.")
         t=await resp.json()
@@ -36,12 +38,12 @@ async def fav_(request:Request,user_id:int=None,authorization: str = Header(None
         
         for query in querys:
             await conn.executemany(query[0],query[1])
-        images=await conn.fetch("""SELECT Images.extension,Tags.name,Tags.id,Tags.is_nsfw,Tags.description,Images.file,Images.dominant_color,Images.source FROM FavImages
+        images=await conn.fetch("""SELECT Images.extension,Tags.name,Tags.id,Tags.is_nsfw,Tags.description,Images.file,Images.dominant_color,Images.source, (SELECT count(FavImages.image) from FavImages where image=Images.file) as like, Images.uploaded_at,FavImages.added_at FROM FavImages
                             JOIN Images ON Images.file=FavImages.image
                             JOIN LinkedTags ON LinkedTags.image=FavImages.image
                             JOIN Tags on LinkedTags.tag_id=Tags.id
                             WHERE not Images.is_banned
-                            and user_id=$1 ORDER BY Images.id DESC""",token_user_id)
+                            and user_id=$1 ORDER BY added_at DESC""",token_user_id)
     if not images and not insert and not delete:
         raise HTTPException(status_code=404,detail="You have no Gallery or it is empty.")
     tags_=db_to_json(images)
