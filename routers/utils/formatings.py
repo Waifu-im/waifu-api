54d47bbab59c44cdf5584ca55b1a8ce0ad655c82
Buco <80671351+Bucolo@users.noreply.github.com>
2021-12-02 01:32:26 +0100

    Update formatings.py

--- routers/utils/formatings.py
+++ routers/utils/formatings.py
@@ -3,15 +3,26 @@ from werkzeug.datastructures import MultiDict
 from fastapi.encoders import jsonable_encoder
 
 class Image:
-    def __init__(self,filename,extension,dominant_color=None):
-        self.filename=filename
+    def __init__(self,**kwargs):
+        for key,value in kwargs.items():
+            setattr(self,key,value)
+        self.url="https://cdn.waifu.im/"+self.file+self.extension
+        
+        
+    def __hash__(self):
+        return int(self.image_id)
+    def __eq__(self,o):
+        return o==self.__hash__()
+
+class PartialImage:
+    def __init__(self,file,extension):
+        self.file=file
         self.extension=extension
-        self.fullfilename=filename+extension
-        self.dominant_color=dominant_color
+        self.filename=self.file+self.extension
 
 class Tags:
     def __init__(self,tag_id,name,is_nsfw,description):
-        self.tag_id=tag_id
+        self.tag_id=int(tag_id)
         self.name=name
         self.is_nsfw=bool(is_nsfw)
         self.description=description
@@ -21,27 +32,40 @@ class Tags:
         return o==self.__hash__()
 
 
-def db_to_json(images):
-    tagmapping=[]
-    for im in images:
-        im=dict(im)
-        im['id']=int(im['id'])
-        tagmapping.append((Tags(im.pop('id'),im.pop('name'),im.pop('is_nsfw'),im.pop('description')),im))
-    tagmapping=MultiDict(tagmapping)
-    default_tags=['ero','all']
-    tags_=[]
-    for tag,image in tagmapping.items():
-        tag_images=tagmapping.getlist(tag.tag_id)
-        tag_images=[dict(t,**{'url':'https://cdn.waifu.im/'+t['file']+t['extension']}) for t in tag_images]
-        tags_.append(dict(vars(tag),**{'images':tag_images}))
-    return jsonable_encoder(tags_)
+def db_to_json(images,tag_mod=False):
+    if tag_mod:
+        tagmapping=[]
+        for im in images:
+            im=jsonable_encoder(im)
+            tagmapping.append((Tags(im.pop('id'),im.pop('name'),im.pop('is_nsfw'),im.pop('description')),im))
+        tagmapping=MultiDict(tagmapping)
+        tags_=[]
+        for tag in tagmapping.keys():
+            tag_images=tagmapping.getlist(tag.tag_id)
+            tag_images=[dict(t,**{'url':'https://cdn.waifu.im/'+t['file']+t['extension']}) for t in tag_images]
+            tags_.append(dict(vars(tag),**{'images':tag_images}))
+        return jsonable_encoder(tags_)
+    else:
+        imagemapping=[]
+        for image in images:
+            image=jsonable_encoder(image)
+            tag=Tags(image.pop('id'),image.pop('name'),image.pop('is_nsfw'),image.pop('description'))
+            imagemapping.append((Image(**image),tag))
+        imagemapping=MultiDict(imagemapping)
+        images_list=[]
+        for im in imagemapping.keys():
+            tags=imagemapping.getlist(im.image_id)
+            images_list.append(dict(vars(im),**{'tags':tags}))
+        return jsonable_encoder(images_list)
+ 
+
 
 def format_to_image(string):
     if not string:
         return []
     images=[x for x in string.split(",")]
     try:
-        return [Image(os.path.splitext(x)[0],os.path.splitext(x)[1]) for x in images]
+        return [PartialImage(os.path.splitext(x)[0],os.path.splitext(x)[1]) for x in images]
     except:
         raise ValueError
 
