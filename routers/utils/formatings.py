92c3cad8d46b6303acb92f9a460e0eec9ee9a7e4
Buco <arnaud@grimbert.com>
2021-12-21 19:45:09 +0100

    change organisation of files a bit

--- routers/utils/formatings.py
+++ routers/utils/formatings.py
@@ -1,41 +1,7 @@
 import os
 from werkzeug.datastructures import MultiDict
 from fastapi.encoders import jsonable_encoder
-
-
-class Image:
-    def __init__(self, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-        self.url = "https://cdn.waifu.im/" + self.file + self.extension
-
-    def __hash__(self):
-        return int(self.image_id)
-
-    def __eq__(self, o):
-        return o == self.__hash__()
-
-
-class PartialImage:
-    def __init__(self, file, extension):
-        self.file = file
-        self.extension = extension
-        self.filename = self.file + self.extension
-
-
-class Tags:
-    def __init__(self, tag_id, name, is_nsfw, description):
-        self.tag_id = int(tag_id)
-        self.name = name
-        self.is_nsfw = bool(is_nsfw)
-        self.description = description
-
-    def __hash__(self):
-        return self.tag_id
-
-    def __eq__(self, o):
-        return o == self.__hash__()
-
+from .types import Image,PartialImage,Tags
 
 def db_to_json(images, tag_mod=False):
     if tag_mod:
--- routers/utils/types.py
+++ routers/utils/types.py
@@ -18,9 +18,43 @@ class ImageQueue:
         async with self._redis.pipeline(transaction=True) as pipe:
             await (
                 pipe.lpush(self.listname, *item if isinstance(item, list) else item)
-                .ltrim(self.listname, 0, self.maxsize - 1)
-                .execute()
+                    .ltrim(self.listname, 0, self.maxsize - 1)
+                    .execute()
             )
 
     async def get(self):
         return await self._redis.lrange(self.listname, 0, -1)
+
+
+class Image:
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        self.url = "https://cdn.waifu.im/" + self.file + self.extension
+
+    def __hash__(self):
+        return int(self.image_id)
+
+    def __eq__(self, o):
+        return o == self.__hash__()
+
+
+class PartialImage:
+    def __init__(self, file, extension):
+        self.file = file
+        self.extension = extension
+        self.filename = self.file + self.extension
+
+
+class Tags:
+    def __init__(self, tag_id, name, is_nsfw, description):
+        self.tag_id = int(tag_id)
+        self.name = name
+        self.is_nsfw = bool(is_nsfw)
+        self.description = description
+
+    def __hash__(self):
+        return self.tag_id
+
+    def __eq__(self, o):
+        return o == self.__hash__()
