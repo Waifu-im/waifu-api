3fad831c60a1789576f455eb4d5a770252cad16e
Buco <arnaud@grimbert.com>
2022-03-10 00:59:13 +0100

    Test fastapi auto docs

--- routers/utils/formatings.py
+++ routers/utils/formatings.py
@@ -38,11 +38,11 @@ def format_tags_where(selected_tags, excluded_tags):
 
 def format_nsfw(is_nsfw):
     string = "Images.is_nsfw"
-    return string if is_nsfw == CustomBool.true or CustomBool.none else 'not ' + string
+    return string if (is_nsfw == CustomBool.true or is_nsfw == CustomBool.none) else 'not ' + string
 
 
 def format_image_type(is_nsfw, tags):
-    if is_nsfw is None or CustomBool.none:
+    if is_nsfw == CustomBool.none:
         return ''
     if tags:
         return f"and ({f'{format_nsfw(is_nsfw)} or ' if is_nsfw is not None else ''}" \
