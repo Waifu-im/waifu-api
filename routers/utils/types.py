eb376a95fca9bb2328a30c2253db7177f005e224
Bucolo <arnaud@grimbert.com>
2022-09-14 01:04:49 +0200

    remove int conversion

--- routers/utils/types.py
+++ routers/utils/types.py
@@ -71,7 +71,7 @@ class Image:
 
 class PartialImage:
     def __init__(self, image_id, extension):
-        self.image_id = int(image_id)
+        self.image_id = image_id
         self.extension = extension
         self.filename = str(self.image_id) + self.extension
 
